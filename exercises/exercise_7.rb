a9bfe60b80ffee7c5cdb97943af98d15ec0e5d1e
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-04 17:06:39 -0500

    Exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "What is the store's name?"
+@store_name = gets.chomp
+
+newStore = Store.create(name: @store_name)
+
+puts "Error: #{newStore.errors.full_messages}"
+
+puts "----------"
+puts "End of Exercise 7"
