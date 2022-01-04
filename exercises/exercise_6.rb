ac63895979a5a4607523514e2162ec8d0ef7f4cb
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-04 16:46:44 -0500

    modify some data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -12,10 +12,10 @@ puts "----------"
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Mojdeh", last_name: "Sabz", hourly_rate: 70)
 @store1.employees.create(first_name: "Ana", last_name: "Ray", hourly_rate: 60)
-@store2.employees.create(first_name: "Richard", last_name: "Downson", hourly_rate: 25)
-@store2.employees.create(first_name: "Jack", last_name: "Ma", hourly_rate: 30)
-@store2.employees.create(first_name: "Sara", last_name: "Jordan", hourly_rate: 30)
-@store2.employees.create(first_name: "Sara", last_name: "Jordan", hourly_rate: 20)
+@store2.employees.create(first_name: "Richard", last_name: "Downson", hourly_rate: 50)
+@store2.employees.create(first_name: "Jack", last_name: "Ma", hourly_rate: 50)
+@store2.employees.create(first_name: "Sara", last_name: "Jordan", hourly_rate: 70)
+@store2.employees.create(first_name: "Sara", last_name: "Jordan", hourly_rate: 60 )
 
 
 puts "----------"
