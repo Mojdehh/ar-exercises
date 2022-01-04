081488ebe0c33388970676d9ee6ede02c7877387
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-04 17:06:09 -0500

    modify hourly_rate data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -15,7 +15,7 @@ puts "----------"
 @store2.employees.create(first_name: "Richard", last_name: "Downson", hourly_rate: 50)
 @store2.employees.create(first_name: "Jack", last_name: "Ma", hourly_rate: 50)
 @store2.employees.create(first_name: "Sara", last_name: "Jordan", hourly_rate: 70)
-@store2.employees.create(first_name: "Sara", last_name: "Jordan", hourly_rate: 60 )
+@store2.employees.create(first_name: "Spencer", last_name: "bawer", hourly_rate: 60)
 
 
 puts "----------"
