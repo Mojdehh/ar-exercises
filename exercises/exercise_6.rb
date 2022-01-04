d9dcb8a1b729165204787e56c16c252fca777bd0
Mojdeh <mojdeh.sbzgh@gmail.com>
2022-01-04 16:43:56 -0500

    exercise6- one-to-many associations, add data to employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Mojdeh", last_name: "Sabz", hourly_rate: 70)
+@store1.employees.create(first_name: "Ana", last_name: "Ray", hourly_rate: 60)
+@store2.employees.create(first_name: "Richard", last_name: "Downson", hourly_rate: 25)
+@store2.employees.create(first_name: "Jack", last_name: "Ma", hourly_rate: 30)
+@store2.employees.create(first_name: "Sara", last_name: "Jordan", hourly_rate: 30)
+@store2.employees.create(first_name: "Sara", last_name: "Jordan", hourly_rate: 20)
+
+
+puts "----------"
+puts "End of Exercise 6"
